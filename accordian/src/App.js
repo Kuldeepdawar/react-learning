bb1ce8c9cd70fb8ee951138948f781f1b7535548
KuldeepDawar <kuldeepdawar47@gmail.com>
2024-02-21 12:43:09 +1100

    add tree view  project

--- accordian/src/App.js
+++ accordian/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import Accordians from "./components/accordians";
 import ImageSlider from "./components/image-slider";
+import LoadApi from "./components/load-products";
 import GenerateRandomColor from "./components/random-generate-color";
 import StarRating from "./components/star-rating/inde";
+import TreeView from "./components/tree-view";
 
 function App() {
   return (
@@ -14,12 +16,15 @@ function App() {
       {/*Start-rating /> */}
       {/*        <StarRating numOfStar={10} /> />
        */}
-
-      <ImageSlider
+      {/*<ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> /> */}
+      {/*LOAD API PRODUCTS /> */}
+      {/*  <LoadApi /> */}
+      {/*Tree View, UI components */}
+      <TreeView />
     </>
   );
 }
